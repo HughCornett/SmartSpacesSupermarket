41e1c0577d3f2507ecc6d6a2d8c5ce5bf58ef8d8
Unknown <giajt001@mymail.unisa.edu.au>
2019-10-30 13:29:01 +0100

    getFullName() added

--- app/src/main/java/com/example/smartspacesblindshopping/Item.java
+++ app/src/main/java/com/example/smartspacesblindshopping/Item.java
@@ -85,6 +85,10 @@ public class Item {
         this.section = section;
     }
 
+    public String getFullName(){
+        return this.getBrandName() + " " + getProductName();
+    }
+
 
     //GETTERS & SETTERS
 
