61d55412443518eb4771a9edfc00607fb9c7c5d8
Unknown <giajt001@mymail.unisa.edu.au>
2019-10-31 12:50:29 +0100

    Shopping activity shelf proximity

--- app/src/main/java/com/example/smartspacesblindshopping/ShoppingActivity.java
+++ app/src/main/java/com/example/smartspacesblindshopping/ShoppingActivity.java
@@ -252,9 +252,6 @@ public class ShoppingActivity extends MyActivity {
 
                                             customItemAdapter.notifyDataSetChanged();
                                         }
-                                    } else if (scannedItem != null && currentItem != null) {
-                                        itemShelfProximityFeedback(scannedItem, currentItem);
-
                                     }
                                     break;
                             }
@@ -281,6 +278,8 @@ public class ShoppingActivity extends MyActivity {
     }
 
 
+
+
     /**
      * Provides audio feedback for the location of an item on the shopping list (item J)
      * compared to the scanned item (Item I)
@@ -303,29 +302,29 @@ public class ShoppingActivity extends MyActivity {
 
                 if (i.getLevel() == j.getLevel()) {
                     if (sectionDifference <= 0)
-                        TTSHandler.speak(j.getProductName() + " is " + (Math.abs(sectionDifference) + 1) + spots + " to the right");
+                        TTSHandler.speak(j.getProductName() + " is " + ((Math.abs(sectionDifference)) == 0 ? 1 : Math.abs(sectionDifference)) + spots + " to the right");
                     if (sectionDifference >= 1)
-                        TTSHandler.speak(j.getProductName() + " is " + (Math.abs(sectionDifference) + 1) + spots + " to the left ");
+                        TTSHandler.speak(j.getProductName() + " is " + (((Math.abs(sectionDifference)) == 0 ? 1 : Math.abs(sectionDifference)) + spots + " to the left "));
                 } else {
                     //directly below
                     if (j.getLevel() == 1 && sectionDifference == 0)
                         TTSHandler.speak(j.getProductName() + " is directly below");
                         //below and to the left
                     else if (j.getLevel() == 1 && sectionDifference < 0)
-                        TTSHandler.speak(j.getProductName() + " is below and " + (Math.abs(sectionDifference)) + spots + " to the right");
+                        TTSHandler.speak(j.getProductName() + " is below and " + ((Math.abs(sectionDifference)) == 0 ? 1 : Math.abs(sectionDifference)) + spots + " to the right");
                         //below and to the right
                     else if (j.getLevel() == 1 && sectionDifference > 0)
-                        TTSHandler.speak(j.getProductName() + " is below and " + (Math.abs(sectionDifference) + 1) + spots + " to the left");
+                        TTSHandler.speak(j.getProductName() + " is below and " + (((Math.abs(sectionDifference)) == 0 ? 1 : Math.abs(sectionDifference)) + spots + " to the left"));
 
                         //directly above
                     else if (j.getLevel() == 0 && sectionDifference == 0)
                         TTSHandler.speak(j.getProductName() + " is directly above");
                         //above and to the right
                     else if (j.getLevel() == 0 && sectionDifference < 0)
-                        TTSHandler.speak(j.getProductName() + " is above and " + (Math.abs(sectionDifference)) + spots + " to the right");
+                        TTSHandler.speak(j.getProductName() + " is above and " + (((Math.abs(sectionDifference)) == 0 ? 1 : Math.abs(sectionDifference)) + spots + " to the right"));
                         //above and to the left
                     else if (j.getLevel() == 0 && sectionDifference > 0)
-                        TTSHandler.speak(j.getProductName() + " is above and " + (Math.abs(sectionDifference) + 1) + spots + "  to the left");
+                        TTSHandler.speak(j.getProductName() + " is above and " + (((Math.abs(sectionDifference)) == 0 ? 1 : Math.abs(sectionDifference)) + spots + "  to the left"));
                 }
             } else {
                 TTSHandler.speak("the item you are looking for is on another shelf");
